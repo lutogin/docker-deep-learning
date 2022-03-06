9a66198fc1dff9e0bcad10a1d4a8ca69a690e3bb
Andrey Lutogin <aluto@softserveinc.com>
2022-03-06 13:25:24 +0200

    docker add env and args

--- docker-nodejs-base/src/app.module.ts
+++ docker-nodejs-base/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'data'),
     }),
--- docker-nodejs-base/src/main.ts
+++ docker-nodejs-base/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get<ConfigService>(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  await app.listen(config.get<string>('PORT'));
 }
 bootstrap();
